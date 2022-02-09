0907eeaa62c5ab174665bf64a45a1298032f33d4
ginsan1997 <96338131+ginsan1997@users.noreply.github.com>
2022-02-10 00:11:14 +0300

    Add files via upload

---
+++ Task-1.js
@@ -0,0 +1,19 @@
+let a = "Hello"
+let b = true
+let c = 123
+console.log("// Сложение // ")
+console.log("1: " + (a + b))
+console.log("2: " + (a + c))
+console.log("3: " + (c + b))
+console.log("// Умножение // ")
+console.log("1: " + (a * b))
+console.log("2: " + (a * c))
+console.log("3: " + (c * b))
+console.log("// Деление // ")
+console.log("1: " + (a * b))
+console.log("2: " + (a * c))
+console.log("3: " + (c * b))
+console.log("// Преобразование // ")
+console.log(!!(a))
+console.log(!!(b))
+console.log(!!(c))
